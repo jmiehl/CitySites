fbc90783f8721907474834ba73116cff1724fb2c
Joe Miehl <joe.miehl@gmail.com>
2024-02-10 11:12:23 -0500

    add varible for textField add textField add button

--- CitySites/ContentView.swift
+++ CitySites/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var query: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            TextField("What're you looking for", text: $query)
+                .textFieldStyle(.roundedBorder)
+            Button {
+                //TODO: implement query
+            } label: {
+                Text("Go")
+            }
         }
         .padding()
     }
