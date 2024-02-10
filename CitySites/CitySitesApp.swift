193aa3f9ae5683f5a58b55c91a877e6ddc74d57c
Joe Miehl <joe.miehl@gmail.com>
2024-02-10 11:05:13 -0500

    Initial Commit

---
+++ CitySites/CitySitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySitesApp.swift
+//  CitySites
+//
+//  Created by Joe Miehl on 2/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CitySitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
