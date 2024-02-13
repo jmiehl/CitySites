c6f06f77a08e823c0442e94232db7002d5d4091e
Joe Miehl <joe.miehl@gmail.com>
2024-02-12 19:07:50 -0500

    add data service, add api call for business search

--- ContentView.swift
+++ ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var query: String = ""
+    var service = DataService()
     
     var body: some View {
         HStack {
@@ -21,9 +22,9 @@ struct ContentView: View {
             }
         }
         .padding()
-        .onAppear(perform: {
-            print(Bundle.main.infoDictionary?["API_KEY"] as? String)
-        })
+        .task {
+            await service.businessSearch()
+        }
    
     }
 }
--- DataService.swift
+++ DataService.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+
+struct DataService {
+    
+    var apiKey = Bundle.main.infoDictionary?["API_KEY"] as? String
+    
+    func businessSearch() async {
+        //1. check if api key exists
+        
+            guard apiKey != nil else {
+            return
+        }
+        
+        //2 . create url
+            if let url = URL(string: "https://api.yelp.com/v3/businesses/search?latitude=39.2387&longitude=77.2794&categories=restaurants&limit=10") {
+        
+        //2. create request
+                var request = URLRequest(url: url)
+                request.addValue("Bearer \(apiKey!)", forHTTPHeaderField: "Authorization")
+                request.addValue("application/json", forHTTPHeaderField: "accept")
+            
+        //3. send request
+                do {
+                    let (data, response) = try await  URLSession.shared.data(for: request)
+                    
+                    print(data)
+                    print(response)
+                    
+                } catch {
+                    print (error)
+                }
+               
+        }
+
+    }
+}
