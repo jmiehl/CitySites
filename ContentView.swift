ee4d84125fd8d54758c5b51f88bf5741533080be
Joe Miehl <joe.miehl@gmail.com>
2024-02-14 21:06:45 -0500

    build list UIview

--- ContentView.swift
+++ ContentView.swift
@@ -8,25 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var businesses = [Business]()
+    
     @State var query: String = ""
     var service = DataService()
     
     var body: some View {
-        HStack {
-            TextField("What're you looking for", text: $query)
-                .textFieldStyle(.roundedBorder)
-            Button {
-                //TODO: implement query
-            } label: {
-                Text("Go")
+        VStack {
+            HStack {
+                TextField("What're you looking for", text: $query)
+                    .textFieldStyle(.roundedBorder)
+                Button {
+                    //TODO: implement query
+                } label: {
+                    Text("Go")
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                        .background(.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                }
+            }
+            List (businesses) {b in
+                Text(b.name ?? "NIL")
             }
         }
-        .padding()
-        .task {
-          let businesses = await service.businessSearch()
+            .padding()
+            .task {
+                businesses = await service.businessSearch()
+            }
         }
-   
-    }
 }
 
 #Preview {
