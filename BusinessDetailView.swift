11c9ffce9957198f94a42012e1b1cd55d7069426
Joe Miehl <joe.miehl@gmail.com>
2024-02-18 20:19:20 -0500

    styled business detail view

--- BusinessDetailView.swift
+++ BusinessDetailView.swift
@@ -12,8 +12,90 @@ struct BusinessDetailView: View {
     var business: Business?
     
     var body: some View {
-        Text(business?.name ?? "")
-        Text(business?.location?.address1 ?? "")
+        VStack (spacing: 0){
+            ZStack (alignment: .trailing){
+                Image("detail-placeholder-image")
+                    .resizable()
+                VStack{
+                    Spacer()
+                    Image("yelp-attribution-image")
+                        .frame(width: 72, height: 36)
+                }
+            }
+            .frame(height: 164)
+            
+            if let isClosed =  business?.is_closed  {
+                
+                ZStack (alignment: .leading){
+                    Rectangle()
+                        .foregroundStyle(isClosed ? .red : .green)
+                    Text(isClosed ? "Closed" : "Open")
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(.horizontal)
+                }
+                .frame(height: 36)
+            }
+            
+            ScrollView {
+                VStack (alignment: .leading, spacing: 0) {
+                    Text(business?.name ?? "")
+                        .font(Font.system(size: 21))
+                        .bold()
+                        .padding(.bottom, 10)
+                        .padding(.top, 16)
+                    
+                    Text("\(business?.location?.address1 ?? ""), \(business?.location?.city ?? "")")
+                    Text("\(business?.location?.state ?? "") \(business?.location?.zip_code ?? ""), \(business?.location?.country ?? "")")
+                        .padding(.bottom, 10)
+                    
+                    Image("regular_\(business?.rating ?? 0)")
+                        .padding(.bottom, 16)
+                    
+                    Divider()
+                       
+                    
+                    HStack {
+                        Image(systemName: "phone")
+                        Text(business?.phone ?? "")
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                        
+                    }
+                    .padding(.vertical, 12)
+                    Spacer()
+                 
+                    Divider()
+                        
+                    
+                    HStack {
+                        Image(systemName: "globe")
+                        Text(business?.url ?? "")
+                            .lineLimit(1)
+                        
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                        
+                    }
+                    .padding(.vertical, 12)
+                    Spacer()
+                    
+                    Divider()
+                    
+                    
+                    HStack {
+                        Image(systemName: "bubble.left.and.bubble.right")
+                        Text("\(business?.review_count ?? 0) reviews")
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                        
+                    }
+                    .padding(.vertical, 12)
+                    Spacer()
+                }
+            }
+            .padding(.horizontal)
+        }
     }
 }
 
--- Model/Business.swift
+++ Model/Business.swift
@@ -15,7 +15,7 @@ struct Business: Decodable, Identifiable {
     var display_phone: String?
     var distance: Double?
     var image_url: String?
-    var is_closed: Bool?
+    var is_closed: Bool? 
     var location: Location?
     var name: String?
     var phone: String?
